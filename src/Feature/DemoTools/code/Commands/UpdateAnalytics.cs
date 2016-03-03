c5b7973326f6288b1f0e49b72b49ddd2fd3f8059
OlegJytnik <olj@sitecore.net>
2016-03-03 17:47:34 +0200

    Demo tools initial commit.

---
+++ src/Feature/DemoTools/code/Commands/UpdateAnalytics.cs
@@ -0,0 +1,17 @@
+﻿namespace Sitecore.Feature.DemoTools.Commands
+{
+  using Sitecore.Shell.Framework.Commands;
+  using Sitecore.Text;
+  using Sitecore.Web.UI.Sheer;
+
+  public class UpdateAnalytics : Command
+  {
+    public override void Execute(CommandContext context)
+    {
+
+      UrlString str = new UrlString(UIUtil.GetUri("control:UpdateAnalytics"));
+
+      SheerResponse.ShowModalDialog(str.ToString());
+    }
+  }
+}
---
+++ src/Feature/DemoTools/code/sitecore/shell/Applications/Dialogs/UpdateAnalytics.cs
@@ -0,0 +1,33 @@
+﻿namespace Sitecore.Feature.DemoTools.sitecore.shell.Applications.Dialogs
+{
+  using System;
+  using Sitecore;
+  using Sitecore.Feature.DemoTools.Repositories;
+  using Sitecore.Globalization;
+  using Sitecore.Jobs;
+  using Sitecore.Web.UI.Sheer;
+
+  class UpdateAnalytics : WizardStatusForm
+  {
+    protected void StartJob()
+    {
+      this.JobHandle = RunUpdateAnalyticsVisitsOnToday().ToString();
+      SheerResponse.Timer("CheckStatus", 500);
+    }
+
+    public static Handle RunUpdateAnalyticsVisitsOnToday()
+    {
+      var options = new JobOptions("UpdateAnalyticsVisitsOnToday", "Visits", "website", new UpdateAnalyticsVisitsRepository(), "Run", new object[] { })
+      {
+        ContextUser = Context.User,
+        ClientLanguage = Language.Parse("en"),
+        AfterLife = TimeSpan.FromSeconds(1),
+        Priority = System.Threading.ThreadPriority.BelowNormal
+      };
+
+      var job = JobManager.Start(options);
+
+      return job.Handle;
+    }
+  }
+}
